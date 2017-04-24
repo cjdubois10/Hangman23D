7e67e80854c2e2b5fde60b6e2f80bd0e7dd79afe
Connor DuBois <cjdubois10@gmail.com>
2017-04-24 10:31:37 -0400

    updating my branch to 72% accuracy and adding charcount

--- src/HangmanPlayer.java
+++ src/HangmanPlayer.java
@@ -2,44 +2,171 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-import javax.swing.text.Position;
 
 /*
-
   Authors (group members): Connor DuBois, Alexander Hsieh, Zachary Paryzek, John Autry
   Email addresses of group members: cdubois2014@my.fit.edu, Ahsieh2014@my.fit.edu, jautry2016@my.fit.edu, zparyzek2012@my.fit.edu
   Group name: 23D
-
   Course: CSE2010
   Section: 3
-
   Description of the overall algorithm:
-
-
 */
 
 public class HangmanPlayer {
+
+	// all of the length dictionaries
+	private final ListByLengthHeader[] sortedDictionary;
+
+	// when first guess is run, we know the size of word, so we can immediately
+	// get which list to search
+	private ListByLengthHeader searchRange;
+
+	// if a guess is wrong, we know what it was and we know to remove it
+	private char previousGuess;
+
+	char guess;
+	LinkedList<Integer> posible;
+	// boolean[] posible;
+	char[] numOfOccurences;
+
+	private LinkedList<Character> hasBeenGuessed;
+
 	/*
 	 * ListByLengthHeader can be a data structure that will be used as a start
 	 * point for each list of words by length. It will store length of the words
 	 * in the list, an array of characters in order of most popular, and a
 	 * linkedlist of strings which will be all the words of that length.
 	 */
-	LinkedList<Character> guessed;
-	ListByLengthHeader[] sortedDictionary;
-	LinkedList<Integer> avalible;
-	// boolean[] posible;
-	char previousGuess;
-
 	class ListByLengthHeader {
-		int length;
 		char[] mostPopularChars;
-		LinkedList<String> words;
+		String[] words;
+		ArrayList<ArrayList<String>> wordsWithLetter;
 
-		public ListByLengthHeader(int length, char[] mostPopularChars, LinkedList<String> words) {
-			this.length = length;
+		public ListByLengthHeader(char[] mostPopularChars, String[] words, ArrayList<ArrayList<String>> wordsWithLetter) {
 			this.mostPopularChars = mostPopularChars;
 			this.words = words;
+			this.wordsWithLetter = wordsWithLetter;
+		}
+
+		// returns untouched copy
+		public ListByLengthHeader copy() {
+			return new ListByLengthHeader( mostPopularChars, words, wordsWithLetter);
+		}
+
+		public void calcPopularity() {
+			// System.out.println("word list we get pop chars from: " +
+			// words.toString());
+			// every word in this list in one string
+			String allWords = "";
+
+			// Each word is concatenated into the allWords string
+			for (int i = 0; i < words.length; i++) {
+				allWords += words[i];
+			}
+
+			// initially the array is mapped a-z equals 0-25
+			CharCount[] letters = new CharCount[26];
+
+			for (int i = 0; i < letters.length; i++) {
+				// a is 97 in unicode, and letters increment from there
+				// so get char offset from 97
+				char letter = (char) (97 + i);
+
+				// create new charcount with count 0
+				letters[i] = new CharCount(letter);
+			}
+
+			for (int i = 0; i < allWords.length(); i++) {
+				// get char at position
+				char charAt = allWords.charAt(i);
+
+				// get position in alphabet of char
+				int letterPos = (int) charAt - 97;
+
+				// update that object's count (it know's its char)
+				letters[letterPos].count++;
+			}
+
+			List<CharCount> lettersList = Arrays.asList(letters);
+			// sort in ascending order (smallest letter counts first)
+			Collections.sort(lettersList);
+
+			// reverse so largest counts are first
+			Collections.reverse(lettersList);
+			// System.out.println(lettersList.toString());
+
+			// size of letter list may not equal mostpopchars
+			for (int i = 0; i < mostPopularChars.length; i++) {
+				// assign mostPopularChar to the letters
+				mostPopularChars[i] = lettersList.get(i).letter;
+			}
+			// System.out.println(Arrays.toString(mostPopularChars));
+		}
+
+		// return the most popular char for this list's words
+		public char getMostPop() {
+			return mostPopularChars[0];
+		}
+
+		// return the most popular char for this list's words
+		public char getPop(int index) {
+			return mostPopularChars[index];
+		}
+
+		// remove the first popular and make second place the new most popular
+		public void removeMostPop() {
+			mostPopularChars = Arrays.copyOfRange(mostPopularChars, 1, mostPopularChars.length);
+		}
+
+		// if a guess is correct, and letter's positions is known
+		// then reduce range based on known letter and positions (could be
+		// multiple)
+		public void reduceIncluded(char c, List<Integer> positions) {
+
+			// because its a linked list we cant delete as we iterate
+			// we must mark for delete and then delete after we iterate
+			ArrayList<String> markedForDeletion = new ArrayList<String>();
+
+			// go through all words in list
+			for (String word : words) {
+				// check all positions
+				for (Integer pos : positions) {
+					// if it doesnt have that letter at that position, remove it
+					if (word.charAt(pos) != c) {
+						// mark for deletion
+						markedForDeletion.add(word);
+					}
+				}
+			}
+
+			// go through all words marked for deletion
+			for (String word : markedForDeletion) {
+				// remove them from list
+				// TODO figure out to remove item from array
+				// words.remove(word);
+			}
+
+			// recalc char popularity after word list updated
+			calcPopularity();
+		}
+
+		// if a guess is incorrect, then remove all words containing that guess
+		// letter
+		public void reduceExcluded(char c) {
+			// because its a linked list we cant delete as we iterate
+			// we must mark for delete and then delete after we iterate
+			ArrayList<String> markedForDeletion = new ArrayList<String>();
+
+			// go through all words in this list
+			for (String word : words) {
+				// if it contains incorrect guess (bad letter)
+				if (word.contains(String.valueOf(c))) {
+					// mark for deletion
+					markedForDeletion.add(word);
+				}
+			}
+			// recalc char popularity after word list updated
+			calcPopularity();
 		}
 	}
 
@@ -51,14 +178,36 @@ public class HangmanPlayer {
 																		// of
 																		// word
 																		// file
-		sortedDictionary = new ListByLengthHeader[25]; // create array of
+		sortedDictionary = new ListByLengthHeader[24]; // create array of
 														// ListByLengthHeaders.
 		// There will be 25 for words 1-25 on length.
 
 		// for loop to populate the sortedDictionary with the 24 headers
-		for (int x = 1; x <= 24; x++) {
-			sortedDictionary[x] = new ListByLengthHeader(x, new char[3], new LinkedList<String>());
-		}
+		sortedDictionary[0] = new ListByLengthHeader( new char[26], new String[52], new ArrayList<ArrayList<String>>());
+		sortedDictionary[1] = new ListByLengthHeader( new char[26], new String[155], new ArrayList<ArrayList<String>>());
+		sortedDictionary[2] = new ListByLengthHeader( new char[26], new String[1351], new ArrayList<ArrayList<String>>());
+		sortedDictionary[3] = new ListByLengthHeader( new char[26], new String[5110], new ArrayList<ArrayList<String>>());
+		sortedDictionary[4] = new ListByLengthHeader( new char[26], new String[9987], new ArrayList<ArrayList<String>>());
+		sortedDictionary[5] = new ListByLengthHeader( new char[26], new String[17477], new ArrayList<ArrayList<String>>());
+		sortedDictionary[6] = new ListByLengthHeader( new char[26], new String[23734], new ArrayList<ArrayList<String>>());
+		sortedDictionary[7] = new ListByLengthHeader( new char[26], new String[29926], new ArrayList<ArrayList<String>>());
+		sortedDictionary[8] = new ListByLengthHeader( new char[26], new String[32380], new ArrayList<ArrayList<String>>());
+		sortedDictionary[9] = new ListByLengthHeader( new char[26], new String[30867], new ArrayList<ArrayList<String>>());
+		sortedDictionary[10] = new ListByLengthHeader( new char[26], new String[26011], new ArrayList<ArrayList<String>>());
+		sortedDictionary[11] = new ListByLengthHeader( new char[26], new String[20460], new ArrayList<ArrayList<String>>());
+		sortedDictionary[12] = new ListByLengthHeader( new char[26], new String[14938], new ArrayList<ArrayList<String>>());
+		sortedDictionary[13] = new ListByLengthHeader( new char[26], new String[9762], new ArrayList<ArrayList<String>>());
+		sortedDictionary[14] = new ListByLengthHeader( new char[26], new String[5924], new ArrayList<ArrayList<String>>());
+		sortedDictionary[15] = new ListByLengthHeader( new char[26], new String[3377], new ArrayList<ArrayList<String>>());
+		sortedDictionary[16] = new ListByLengthHeader( new char[26], new String[1813], new ArrayList<ArrayList<String>>());
+		sortedDictionary[17] = new ListByLengthHeader( new char[26], new String[842], new ArrayList<ArrayList<String>>());
+		sortedDictionary[18] = new ListByLengthHeader( new char[26], new String[428], new ArrayList<ArrayList<String>>());
+		sortedDictionary[19] = new ListByLengthHeader( new char[26], new String[198], new ArrayList<ArrayList<String>>());
+		sortedDictionary[20] = new ListByLengthHeader( new char[26], new String[82], new ArrayList<ArrayList<String>>());
+		sortedDictionary[21] = new ListByLengthHeader( new char[26], new String[41], new ArrayList<ArrayList<String>>());
+		sortedDictionary[22] = new ListByLengthHeader( new char[26], new String[17], new ArrayList<ArrayList<String>>());
+		sortedDictionary[23] = new ListByLengthHeader( new char[26], new String[5], new ArrayList<ArrayList<String>>());
+
 		String currentWord; // string representing the word that is read in
 		int currentWordLength; // int to hold the length of the word
 
@@ -69,73 +218,18 @@ public class HangmanPlayer {
 		while (unsortedDictionary.hasNextLine()) {
 			currentWord = unsortedDictionary.nextLine().toLowerCase();
 			currentWordLength = currentWord.length();
-			sortedDictionary[currentWordLength].words.add(currentWord);
+			int i = 0;
+			while (sortedDictionary[currentWordLength - 1].words[i] != null) {
+				i++;
+			}
+			sortedDictionary[currentWordLength - 1].words[i] = (currentWord);
 		}
 
-		for (int i = 1; i <= 24; i++) { // letter count array
-			String allWords = ""; // used to concatenate all words into one
-									// string henceforth called the superString
-									// in comments
-			int allWordsLength = 0; // size of string mentioned above
-			char[] mostPopularChar = new char[] { 'a', 'a', 'a' }; // used for
-																	// sorting
-																	// before
-																	// passing
-																	// to main
-																	// array
-
-			// Each word is concatenated into the supperString
-			for (int j = 0; j < sortedDictionary[i].words.size(); j++) {
-				allWords = allWords + sortedDictionary[i].words.get(j);
-			}
-
-			allWordsLength = allWords.length();
-			// create a map of all the alphabets to store the frequency of each
-			// alphabet
-			// the Key is the alphabet and the frequency is the Value
-			Map<Character, Integer> numChars = new HashMap<Character, Integer>(Math.min(allWordsLength, 26));
-
-			// count the frequency of each alphabet by increasing the count on
-			// each alphabet
-			for (int k = 0; k < allWordsLength; k++) {
-				char charAt = allWords.charAt(k);
-				if (!numChars.containsKey(charAt)) {
-					numChars.put(charAt, 1);
-				} else {
-					numChars.put(charAt, numChars.get(charAt) + 1);
-				}
-			}
-
-			// sorting begins here
-			// if new key is most frequent then it pushes down everything in the
-			// array and takes position 0 (henceforth [0])
-			// if new key if more frequent than [0] but less than [1], pushes
-			// down [1] and replaces [1] with the new key
-			// if new key if more frequent than [1] but less than [2], loop
-			// replaces [2] with new key
-			// only case left is if key is less frequent than all [0],[1], and
-			// [2], so it does nothing and continues the loop.
-			for (Character key : numChars.keySet()) {
-				if (numChars.get(key) >= numChars.get(mostPopularChar[0])) {
-					mostPopularChar[1] = mostPopularChar[0];
-					mostPopularChar[2] = mostPopularChar[1];
-					mostPopularChar[0] = key;
-				} else if (numChars.get(key) <= numChars.get(mostPopularChar[0])
-						&& numChars.get(key) > numChars.get(mostPopularChar[1])) {
-					mostPopularChar[2] = mostPopularChar[1];
-					mostPopularChar[1] = key;
-				} else if (numChars.get(key) <= numChars.get(mostPopularChar[1])
-						&& numChars.get(key) > numChars.get(mostPopularChar[2])) {
-					mostPopularChar[2] = key;
-				}
-			}
-			sortedDictionary[i].mostPopularChars = mostPopularChar; // pushes
-																	// back
-																	// temporary
-																	// sorting
-																	// array to
-																	// main
-																	// array
+		for (int i = 0; i < 24; i++) {
+			sortedDictionary[i].calcPopularity();
+		}
+		for (int i = 0; i < sortedDictionary.length; i++) {
+			System.out.println(sortedDictionary[i].words.length);
 		}
 
 	}
@@ -146,25 +240,32 @@ public class HangmanPlayer {
 	// word
 	// isNewWord: indicates a new hidden word
 	// returns the guessed letter
-
-	// To save on time since java automaticaly sets all boolean arrays to false,
-	// true == false and vise versa
 	public char guess(String currentWord, boolean isNewWord) {
-		char guess = ' ';
-		// is new word resets;
+
+		// System.out.println("********COMMENCE GUESS********");
+
+		// if its our first guess on new word and we dont know length (we dont
+		// know search range)
 		if (isNewWord) {
-			guessed = new LinkedList<>();
-			avalible = new LinkedList<>();
-			for (int i = 0; i < sortedDictionary[currentWord.length()].words.size(); i++) {
-				avalible.add(i);
+			//EvalHangmanPlayer.cnt++;
+			//System.out.println(EvalHangmanPlayer.cnt);
+			// System.out.println(Arrays.toString(sortedDictionary[currentWord.length()-1].words));
+			posible = new LinkedList<>();
+			for (int i = 0; i < sortedDictionary[currentWord.length() - 1].words.length; i++) {
+				posible.add(i);
 			}
-			guess = sortedDictionary[currentWord.length()].mostPopularChars[0];
-			guessed.add(guess);
+			numOfOccurences = sortedDictionary[currentWord.length() - 1].mostPopularChars;
+			guess = numOfOccurences[0];
+			hasBeenGuessed = new LinkedList<Character>();
+			// System.out.println(Arrays.toString(sortedDictionary[currentWord.length()
+			// - 1].mostPopularChars));
 		} else {
-			guess = previousGuess;
-			guessed.add(guess);
+			guess = numOfOccurences[0];
 		}
-		previousGuess = guess;
+		// System.out.println(Arrays.toString(numOfOccurences));
+
+		hasBeenGuessed.add(guess);
+		// System.out.println(hasBeenGuessed.toString());
 		return guess;
 	}
 
@@ -179,48 +280,87 @@ public class HangmanPlayer {
 	// last letter needed
 	// b. false partial word without the guessed letter
 	public void feedback(boolean isCorrectGuess, String currentWord) {
-		if (isCorrectGuess) {
-			char[] temp = currentWord.toCharArray();
-			for (int i = (avalible.size()-1); i >= 0; i--) {
-				String toCheck = sortedDictionary[currentWord.length()].words.get(avalible.get(i));
-				for (int j = 0; j < temp.length; j++) {
-					//System.out.println(temp[j] + " : " + toCheck.charAt(j));
-					if (temp[j] != ' ') {
-						if (temp[j] != toCheck.charAt(j)) {
-							avalible.remove(i);
+		// System.out.println(isCorrectGuess);
+		String allWords = "";
+		// System.out.println("\n\n");
+		char[] currentChar = currentWord.toCharArray();
+
+		for (int k = posible.size() - 1; k >= 0; k--) {
+			// System.out.println(k);
+			// System.out.println("-------" + k + "--------");
+			String temp = sortedDictionary[currentWord.length() - 1].words[posible.get(k)];
+			char[] stringToChar = temp.toCharArray();
+			boolean isStillValid = true;
+
+			if (isStillValid) {
+				for (int j = 0; j < stringToChar.length; j++) {
+					if (isCorrectGuess) {
+						if (currentChar[j] != ' ') {
+							if (currentChar[j] != stringToChar[j]) {
+								posible.remove(k);
+								isStillValid = false;
+								break;
+							}
+						} else {
+							if (stringToChar[j] == guess) {
+								posible.remove(k);
+								isStillValid = false;
+								break;
+							}
+						}
+						
+					} else {
+						if (stringToChar[j] == guess) {
+							posible.remove(k);
+							isStillValid = false;
 							break;
 						}
-					} else if (toCheck.charAt(j) == previousGuess){
-						avalible.remove(i);
-						break;
-						
-					}
-				}
-
-			}
-		} else {
-			for (int i = avalible.size()-1; i >= 0; i--) {
-				String toCheck = sortedDictionary[currentWord.length()].words.get(avalible.get(i));
-				for (int j = 0; j < toCheck.length(); j++) {
-					if (toCheck.charAt(j) == previousGuess) {
-						avalible.remove(i);
-						break;
 					}
 				}
 			}
+			if (isStillValid) {
+				allWords += temp;
+			}
+
 		}
-		String findChar = sortedDictionary[currentWord.length()].words.get(avalible.get(0));
-		//System.out.println(findChar);
-		for (int i = 0; i < currentWord.length(); i++) {
-			char find = findChar.charAt(i);
-			if (!guessed.contains(find)) {
-				System.out.println(find);
-				previousGuess = find;
-				break;
+		// initially the array is mapped a-z equals 0-25
+		CharCount[] letters = new CharCount[26];
+
+		for (int i = 0; i < letters.length; i++) {
+			// a is 97 in unicode, and letters increment from there
+			// so get char offset from 97
+			char letter = (char) (97 + i);
+
+			// create new charcount with count 0
+			letters[i] = new CharCount(letter);
+		}
+
+		for (int i = 0; i < allWords.length(); i++) {
+			// get char at position
+			char charAt = allWords.charAt(i);
+
+			// get position in alphabet of char
+			int letterPos = (int) charAt - 97;
+
+			// update that object's count (it know's its char)
+			letters[letterPos].count++;
+			if (hasBeenGuessed.contains(charAt)) {
+				letters[letterPos].count = 0;
 			}
 		}
 
-		//System.out.println(currentWord + " : " + isCorrectGuess + " : " + avalible.size());
-		//System.out.println(guessed.toString());
+		List<CharCount> lettersList = Arrays.asList(letters);
+		// sort in ascending order (smallest letter counts first)
+		Collections.sort(lettersList);
+
+		// reverse so largest counts are first
+		//Collections.reverse(lettersList);
+		// System.out.println(lettersList.toString());
+
+		// size of letter list may not equal mostpopchars
+		for (int i = 0; i < numOfOccurences.length; i++) {
+			// assign mostPopularChar to the letters
+			numOfOccurences[i] = lettersList.get(i).letter;
+		}
 	}
 }
